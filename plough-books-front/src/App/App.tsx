b7a3ff54e2f411dc15bab817205372b2f359058a
miles <miles.loveday@gmail.com>
2018-10-10 10:56:51 +0100

    Routing & nav bar TODO: wire up routing to redux

--- plough-books-front/src/App/App.tsx
+++ plough-books-front/src/App/App.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import {Route} from "react-router";
+import {Link} from "react-router-dom";
 import {CashUp} from "../DataEntry/CashUp/CashUp";
 import {Rota} from "../DataEntry/Rota/Rota";
 import {SignIn} from "../DataEntry/SignIn/SignIn";
@@ -12,10 +14,20 @@ export class App extends React.Component {
         <header className="App-header">
           <h1 className="App-title">Plough Books</h1>
         </header>
-        <div><CashUp/></div>
-        <div><Rota/></div>
-        <div><SignIn/></div>
-        <div><WeeklyOverview/></div>
+        <nav className="App-nav">
+          <ul className="App-nav-list">
+            <li className="App-nav-item"><Link to="/cash-up">Cash up</Link></li>
+            <li className="App-nav-item"><Link to="/rota">Rota</Link></li>
+            <li className="App-nav-item"><Link to="/sign-in-sheet">Sign-in sheet</Link></li>
+            <li className="App-nav-item"><Link to="/weekly-overview">Weekly overview</Link></li>
+          </ul>
+        </nav>
+        <div className="App-content-container">
+            <Route path="/cash-up" component={CashUp}/>
+            <Route path="/rota" component={Rota}/>
+            <Route path="/sign-in-sheet" component={SignIn}/>
+            <Route path="/weekly-overview" component={WeeklyOverview}/>
+        </div>
       </div>
     );
   }
--- plough-books-front/src/index.tsx
+++ plough-books-front/src/index.tsx
@@ -1,11 +1,25 @@
+import {createBrowserHistory} from 'history';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {Route, Router} from 'react-router';
+import {combineReducers, createStore} from "redux";
 import {App} from './App/App';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
+const store = createStore(combineReducers({
+    // TODO add some reducers
+}));
+
+const browserHistory = createBrowserHistory();
+
 ReactDOM.render(
-  <App />,
+    <Provider store={store}>
+        <Router history={browserHistory}>
+            <Route path="/" component={App}/>
+        </Router>
+    </Provider>,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
