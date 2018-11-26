836cf55e865ee2c162576229f9741fec2eafe60a
Miles Loveday <miles.loveday@gmail.com>
2018-11-26 20:49:21 +0000

    Fetch and display staff roles in data entry page

--- plough-books-front/src/DataEntry/StaffRoles/StaffRoles.tsx
+++ plough-books-front/src/DataEntry/StaffRoles/StaffRoles.tsx
@@ -1,31 +1,61 @@
 import * as React from "react";
 import {connect} from "react-redux";
 import {AppState} from "../../redux";
+import {StaffRolesExternalState} from "./State/StaffRolesExternalState";
+import {StaffRolesLocalState} from "./State/StaffRolesLocalState";
+import {staffRolesFetch} from "./State/StaffRolesRedux";
 
 interface StaffRolesOwnProps {
 }
 
 interface StaffRolesStateProps {
+  staffRolesExternalState: StaffRolesExternalState;
+  staffRolesLocalState: StaffRolesLocalState;
 }
 
 const mapStateToProps = (state: AppState, ownProps: StaffRolesOwnProps): StaffRolesStateProps => {
-  return {}
+  return {
+    staffRolesExternalState: state.staffRolesExternalState,
+    staffRolesLocalState: state. staffRolesLocalState,
+  }
 };
 
 interface StaffRolesDispatchProps {
+  fetchStaffRoles: () => void;
 }
 
 const mapDispatchToProps = (dispatch: any, ownProps: StaffRolesOwnProps): StaffRolesDispatchProps => {
-  return {};
+  return {
+    fetchStaffRoles: () => dispatch(staffRolesFetch()),
+  };
 };
 
 type StaffRolesProps = StaffRolesOwnProps & StaffRolesStateProps & StaffRolesDispatchProps;
 
 class StaffRolesComponent extends React.Component<StaffRolesProps, {}> {
+  public componentDidMount() {
+    this.maintainStateWithUrl();
+  }
+
+  public componentDidUpdate() {
+    this.maintainStateWithUrl();
+  }
+
   public render() {
     return (
-      <div>Staff roles data entry</div>
+      <div>
+        {this.props.staffRolesLocalState.roles.map((role, key) => (
+          <div key={key}>{role.role}</div>
+        ))}
+      </div>
     )
+  }
+
+  private maintainStateWithUrl() {
+    if (this.props.staffRolesExternalState.isEmpty()) {
+      this.props.fetchStaffRoles();
+      return;
+    }
   }
 }
 
