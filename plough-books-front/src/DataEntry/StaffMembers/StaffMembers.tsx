530842d46cdc0320e408feb97b21537d4302af43
Miles Loveday <miles.loveday@gmail.com>
2018-11-26 20:52:07 +0000

    Fetch and display staff members in data entry page

--- plough-books-front/src/DataEntry/StaffMembers/StaffMembers.tsx
+++ plough-books-front/src/DataEntry/StaffMembers/StaffMembers.tsx
@@ -1,31 +1,61 @@
 import * as React from "react";
 import {connect} from "react-redux";
 import {AppState} from "../../redux";
+import {StaffMembersExternalState} from "./State/StaffMembersExternalState";
+import {StaffMembersLocalState} from "./State/StaffMembersLocalState";
+import {staffMembersFetch} from "./State/StaffMembersRedux";
 
 interface StaffMembersOwnProps {
 }
 
 interface StaffMembersStateProps {
+  staffMembersExternalState: StaffMembersExternalState;
+  staffMembersLocalState: StaffMembersLocalState;
 }
 
 const mapStateToProps = (state: AppState, ownProps: StaffMembersOwnProps): StaffMembersStateProps => {
-  return {}
+  return {
+    staffMembersExternalState: state.staffMembersExternalState,
+    staffMembersLocalState: state. staffMembersLocalState,
+  }
 };
 
 interface StaffMembersDispatchProps {
+  fetchStaffMembers: () => void;
 }
 
 const mapDispatchToProps = (dispatch: any, ownProps: StaffMembersOwnProps): StaffMembersDispatchProps => {
-  return {};
+  return {
+    fetchStaffMembers: () => dispatch(staffMembersFetch()),
+  };
 };
 
 type StaffMembersProps = StaffMembersOwnProps & StaffMembersStateProps & StaffMembersDispatchProps;
 
 class StaffMembersComponent extends React.Component<StaffMembersProps, {}> {
+  public componentDidMount() {
+    this.maintainStateWithUrl();
+  }
+
+  public componentDidUpdate() {
+    this.maintainStateWithUrl();
+  }
+  
   public render() {
     return (
-      <div>Staff members data entry</div>
+      <div>
+        {this.props.staffMembersLocalState.members.map((member, key) => (
+          <div key={key}>{member.name}</div>
+        ))}
+      </div>
     )
+  }
+
+  private maintainStateWithUrl() {
+    if (this.props.staffMembersExternalState.isEmpty()) {
+      this.props.fetchStaffMembers();
+      return;
+    }
   }
 }
 
