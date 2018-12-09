05aec1c577a80894250812f0b97665d7b58b4405
Miles Loveday <miles.loveday@gmail.com>
2018-12-09 20:03:34 +0000

    Remove unused environment in front end config

--- plough-books-back/assets/env/Config.ts
+++ plough-books-back/assets/env/Config.ts
@@ -5,9 +5,6 @@ switch (process.env.environment) {
     case 'production':
         backendDomain = 'https://dashboard.theploughharborne.co.uk';
         break;
-    case 'remote-dev':
-        backendDomain = 'https://plough-dash.duckdns.org';
-        break;
     case 'dev':
     default:
         backendDomain = 'http://localhost:8000';
