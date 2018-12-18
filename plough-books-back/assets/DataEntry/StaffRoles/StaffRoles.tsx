693d3d98e8764939dca6e5a96cde9a6cb8d55183
Miles Loveday <miles.loveday@gmail.com>
2018-12-18 06:50:10 +0000

    Staff Roles pick statuses from list; Fix broken styling on staff role edit/save/cancel buttons

--- plough-books-back/assets/DataEntry/StaffRoles/StaffRoles.tsx
+++ plough-books-back/assets/DataEntry/StaffRoles/StaffRoles.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import {connect} from "react-redux";
+import {StaffRoleStatus} from "../../Enum/StaffRoleStatus";
 import {WorkTypes} from "../../Enum/WorkTypes";
 import {AppState} from "../../redux";
 import {getStaffRoleOrder} from "../../Util/SortingUtils";
@@ -65,16 +66,22 @@ class StaffRolesComponent extends React.Component<StaffRolesProps, {}> {
             return (
               <div className="staff-role-entity" key={key}>
                 <input disabled={!isEditingThisRole} value={role.role} onChange={ev => this.updateStaffRole(role.with({'role': ev.target.value}))}/>
-                <input disabled={!isEditingThisRole} value={role.status} onChange={ev => this.updateStaffRole(role.with({'status': ev.target.value}))}/>
+                <select disabled={!isEditingThisRole} value={role.status} onChange={ev => this.updateStaffRole(role.with({'status': ev.target.value}))}>
+                  <option value={StaffRoleStatus.ACTIVE}>Active</option>
+                  <option value={StaffRoleStatus.INACTIVE}>Inactive</option>
+                  <option value={StaffRoleStatus.IMPORTED}>(imported)</option>
+                </select>
                 <input disabled={!isEditingThisRole} type='number' value={role.orderInRota} step={1} onChange={ev => this.updateStaffRole(role.with({'orderInRota': ev.target.value}))}/>
                 <select disabled={!isEditingThisRole} value={role.type} onChange={ev => this.updateStaffRole(role.with({type: ev.target.value}))}>
                     <option value='bar'>Bar</option>
                     <option value='kitchen'>Kitchen</option>
                 </select>
-                {!isCreatingNewRole && !isEditingThisRole && !this.props.staffRolesLocalState.isEditing() &&
-                <button type='button' onClick={() => this.updateStaffRole(role)}>Edit</button>}
-                {isEditingThisRole && <button type='button' onClick={() => this.saveStaffRole(role)}>Save</button>}
-                {isEditingThisRole && <button type='button' onClick={() => this.cancelEdit()}>Cancel</button>}
+                <div className="staff-role-edit-buttons">
+                  {!isCreatingNewRole && !isEditingThisRole && !this.props.staffRolesLocalState.isEditing() &&
+                  <button type='button' onClick={() => this.updateStaffRole(role)}>Edit</button>}
+                  {isEditingThisRole && <button type='button' onClick={() => this.saveStaffRole(role)}>Save</button>}
+                  {isEditingThisRole && <button type='button' onClick={() => this.cancelEdit()}>Cancel</button>}
+                </div>
               </div>
             )
           }
@@ -91,10 +98,12 @@ class StaffRolesComponent extends React.Component<StaffRolesProps, {}> {
             <option value={WorkTypes.BAR}>Bar</option>
             <option value={WorkTypes.KITCHEN}>Kitchen</option>
           </select>}
-          {!isCreatingNewRole && !this.props.staffRolesLocalState.isEditing() &&
-          <button type='button' onClick={() => this.newStaffRole()}>New</button>}
-          {isCreatingNewRole && <button type='button' onClick={() => this.saveStaffRole(this.props.staffRolesLocalState.newRole)}>Save</button>}
-          {isCreatingNewRole && <button type='button' onClick={() => this.cancelEdit()}>Cancel</button>}
+          <div className="staff-role-edit-buttons">
+            {!isCreatingNewRole && !this.props.staffRolesLocalState.isEditing() &&
+            <button type='button' onClick={() => this.newStaffRole()}>New</button>}
+            {isCreatingNewRole && <button type='button' onClick={() => this.saveStaffRole(this.props.staffRolesLocalState.newRole)}>Save</button>}
+            {isCreatingNewRole && <button type='button' onClick={() => this.cancelEdit()}>Cancel</button>}
+          </div>
         </div>
       </div>
     )
---
+++ plough-books-back/assets/Enum/StaffRoleStatus.ts
@@ -0,0 +1,5 @@
+export enum StaffRoleStatus {
+  IMPORTED = 'import',
+  INACTIVE = 'inactive',
+  ACTIVE = 'active',
+}
