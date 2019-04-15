ee330e0b04c3c9da72640d0bbf9908ad644e811a
miles <miles.loveday@gmail.com>
2019-04-15 19:58:44 +0100

    Formatting of cash from a number removes whitespace around the result

--- code/assets/Util/Formatting.ts
+++ code/assets/Util/Formatting.ts
@@ -2,9 +2,9 @@ export class Formatting {
   public static formatCash(value: number, includePoundSign: boolean = true): string {
     const isNegative = value < 0;
     const sign = isNegative ? '-' : '';
-    return `${sign} ${includePoundSign ? '£' : ''}${Math.abs(value).toLocaleString('en', {maximumFractionDigits: 2})}`;
+    return `${sign}${includePoundSign ? ' £' : ''}${Math.abs(value).toLocaleString('en', {maximumFractionDigits: 2})}`.trim();
   }
   public static formatPercent(value: number, decimals: number = 2, includePercent: boolean = true): string {
-    return (100*value).toLocaleString('en', {maximumFractionDigits: decimals})+(includePercent ? '%' : '');
+    return (100*value).toLocaleString('en', {maximumFractionDigits: decimals})+(includePercent ? '%' : '').trim();
   }
 }
