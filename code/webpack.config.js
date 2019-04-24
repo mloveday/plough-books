f0efd8c3898812e63752b8b4e45cce2e427ed379
miles <miles.loveday@gmail.com>
2019-04-24 20:05:33 +0100

    Unbreak broken dev build tools

--- code/webpack.config.js
+++ code/webpack.config.js
@@ -16,25 +16,45 @@ Encore
     .enableReactPreset()
     .copyFiles({
         from: './assets/images',
-        // optional target path, relative to the output dir
-        //to: 'images/[path][name].[ext]',
-        // only copy files matching this pattern
-        //pattern: /\.(png|jpg|jpeg)$/
     })
+
+// totally not different
+  .enableSourceMaps(true)
 ;
 
 const config = Encore.getWebpackConfig();
 config.resolve.alias = {
   env: path.resolve(Encore.isProduction() ? 'assets/env/prod' : 'assets/env/dev')
 };
-config.useSourceMaps = true;
 config.name = 'default';
+
+// duplicate of above until obvious comment
+Encore.reset();
+Encore
+  .setOutputPath('../public_html/build/')
+  .setPublicPath('/build')
+
+  .addEntry('app', './assets/index.tsx')
+  .enableSingleRuntimeChunk()
+
+  .cleanupOutputBeforeBuild()
+  .enableBuildNotifications()
+  .enableVersioning(Encore.isProduction())
+  .enableSassLoader()
+  .enableTypeScriptLoader()
+  .enableReactPreset()
+  .copyFiles({
+    from: './assets/images'
+  })
+
+// totally not different
+  .enableSourceMaps(false)
+;
 
 const personalConfig = Encore.getWebpackConfig();
 personalConfig.resolve.alias = {
   env: path.resolve('assets/env/personal')
 };
-config.useSourceMaps = false;
 personalConfig.name = 'personal';
 
 
