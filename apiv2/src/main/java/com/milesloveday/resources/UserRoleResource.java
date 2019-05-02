abb3225c5ca0421b6a3b6a174d5d07bdacfdc00a
miles <miles.loveday@gmail.com>
2019-05-02 20:46:29 +0100

    Sample API response for single UserRole endpoint

--- apiv2/src/main/java/com/milesloveday/resources/UserRoleResource.java
+++ apiv2/src/main/java/com/milesloveday/resources/UserRoleResource.java
@@ -4,6 +4,7 @@ import com.milesloveday.api.UserRole;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.util.Collections;
@@ -13,11 +14,21 @@ import java.util.List;
 @Produces(MediaType.APPLICATION_JSON)
 public class UserRoleResource {
     @GET
-    public List<UserRole> allUserRoles() {
+    public List<UserRole> getAllUserRoles() {
         UserRole ur = new UserRole();
         ur.setId(1L);
         ur.setRole("Admin");
         ur.setManagesUsers(true);
         return Collections.singletonList(ur);
+    }
+
+    @GET
+    @Path("{id}")
+    public UserRole getUserRole(@PathParam("id") final long id) {
+        UserRole ur = new UserRole();
+        ur.setId(id);
+        ur.setRole("Admin");
+        ur.setManagesUsers(true);
+        return ur;
     }
 }
