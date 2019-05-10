9bd7a41b591d0a483d66ce16e12e861ce980e44e
miles <miles.loveday@gmail.com>
2019-05-10 10:58:26 +0100

    Sundays are matched when checking if cash ups already exist in local state

--- code/assets/Redux/CashUp/CashUpExternalState.ts
+++ code/assets/Redux/CashUp/CashUpExternalState.ts
@@ -1,6 +1,7 @@
 import * as moment from "moment";
 import {CashUpsForWeek} from "../../Model/CashUp/CashUpsForWeek";
 import {FetchStatus} from "../../Model/Enum/FetchStatus";
+import {DateFormats} from "../../Util/DateFormats";
 import {ExternalState} from "../ExternalState";
 
 export class CashUpExternalState extends ExternalState {
@@ -18,7 +19,7 @@ export class CashUpExternalState extends ExternalState {
     }
 
     public shouldLoadForDate(date: moment.Moment) {
-    return this.isEmpty()
-      || (this.isLoaded() && !this.cashUpsForWeek.cashUps.find(cashUp => date.isSame(cashUp.date, 'day')));
+      return this.isEmpty()
+        || (this.isLoaded() && !this.cashUpsForWeek.cashUps.find(cashUp => date.format(DateFormats.API_DATE) === cashUp.date));
   }
 }
